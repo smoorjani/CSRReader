4b2b49af4b87e1c84af1b20bb9b6de094fe16bde
nikwalia <nikash.walia@gmail.com>
2022-02-13 22:54:35 -0600

    Simplify functions, only one file read

--- src/mmio_read.cpp
+++ src/mmio_read.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <torch/extension.h>
+#include <format>
 
 #include "mmio.h"
 #define MAX(X, Y) (((X) > (Y)) ? (X) : (Y))
@@ -46,8 +47,15 @@ void read_mmio(std::string filename, T_idx* I, T_idx* J, T_val* val) {
     J = (T_idx*) malloc(nz * sizeof(T_idx));
     val = (T_val*) malloc(nz * sizeof(T_val));
 
+    char* parse_string;
+
+    if (delim == ',') {
+        parse_string = "%d,%d,%lg\n";
+    } else {
+        parse_string = "%d\t%d\t%lg\n";
+    }
     for (T_idx i = 0; i < nz; i++) {
-        fscanf(f, "%d %d %lg\n", &I[i], &J[i], &val[i]);
+        fscanf(f, parse_string, &I[i], &J[i], &val[i]);
         I[i]--;  /* adjust from 1-based to 0-based */
         J[i]--;
     }
@@ -59,7 +67,7 @@ void read_mmio(std::string filename, T_idx* I, T_idx* J, T_val* val) {
 
 // TODO: read tsv/csv 
 template <typename T_idx, T_val>
-void read_csv(std::string filename, T_idx* I, T_idx* J, T_val* val) {
+void read_csv(std::string filename, T_idx* I, T_idx* J, T_val* val, T_idx* _M, T_idx* _nnz, char delim=',') {
     const char *c_filename = filename.c_str();
     FILE *f;
     if ((f = fopen(c_filename, "r")) == NULL) {
@@ -70,64 +78,30 @@ void read_csv(std::string filename, T_idx* I, T_idx* J, T_val* val) {
     T_idx tmprow;
     T_idx tmpcol;
     T_idx tmpval;
-    
-    // get filesize
-    while (!feof(f)) {
-        fscanf(f, "%d,%d,%lg\n", &tmprow, &Jtmpcol, &tmpval);
-        nnz += 1;
-        M = MAX(M, tmprow);
-        M = MAX(M, tmpcol);
-    }
-    
+
+    fseek(f, 0, SEEK_END);
+    size_t size = ftell(f);
     fseek(f, 0, SEEK_SET);
 
-    I = (T_idx*) malloc(nz * sizeof(T_idx));
-    J = (T_idx*) malloc(nz * sizeof(T_idx));
-    val = (T_val*) malloc(nz * sizeof(T_val));
+    // 2 indices, 1 value, 2 delimiters, 1 newline
+    *nnz = size / (sizeof(T_idx) * 2 + sizeof(T_val) + sizeof(char) * 3);
+
+    I = (T_idx*) malloc(nnz * sizeof(T_idx));
+    J = (T_idx*) malloc(nnz * sizeof(T_idx));
+    val = (T_val*) malloc(nnz * sizeof(T_val));
 
     for (T_idx i = 0; i < nnz; i++) {
         fscanf(f, "%d,%d,%lg\n", &I[i], &J[i], &val[i]);
+        M = MAX(M, I[i]);
+        M = MAX(M, J[i]);
     }
 
     if (f != stdin) {
         fclose(f);
     }
-}
 
-template <typename T_idx, T_val>
-void read_tsv(std::string filename, T_idx* I, T_idx* J, T_val* val) {
-    const char *c_filename = filename.c_str();
-    FILE *f;
-    if ((f = fopen(c_filename, "r")) == NULL) {
-        throw std::runtime_error("Could not open file");
-    }
-
-    T_idx M, nnz = 0;
-    T_idx tmprow;
-    T_idx tmpcol;
-    T_idx tmpval;
-    
-    // get filesize
-    while (!feof(f)) {
-        fscanf(f, "%d\t%d\t%lg\n", &tmprow, &Jtmpcol, &tmpval);
-        nnz += 1;
-        M = MAX(M, tmprow);
-        M = MAX(M, tmpcol);
-    }
-    
-    fseek(f, 0, SEEK_SET);
-
-    I = (T_idx*) malloc(nz * sizeof(T_idx));
-    J = (T_idx*) malloc(nz * sizeof(T_idx));
-    val = (T_val*) malloc(nz * sizeof(T_val));
-
-    for (T_idx i = 0; i < nnz; i++) {
-        fscanf(f, "%d\t%d\t%lg\n", &I[i], &J[i], &val[i]);
-    }
-
-    if (f != stdin) {
-        fclose(f);
-    }
+    *_M = M;
+    *_nnz = nnz;
 }
 
 template <typename T_idx>
